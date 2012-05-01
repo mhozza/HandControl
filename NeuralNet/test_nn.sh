a34cc745af5f7763b20af85537168a8d40b328b6
Michal Hozza <mhozza@gmail.com>
2012-05-01 11:23:30 +0200

    pridane univerzsalne trenovacie a testovacie skripty pre architektury

---
+++ NeuralNet/test_nn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./NeuralNetNN_$1 1 0 3 ../hand_images/test/hands_old ../hand_images/test/other_old train_nn_$1.weights
+
---
+++ NeuralNet/train_nn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./NeuralNetNN_$1 0 0 3 ../hand_images/hands_old ../hand_images/other_old train_nn_$1.weights
+
