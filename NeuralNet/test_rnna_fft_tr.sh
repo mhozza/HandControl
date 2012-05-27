1e05a8b24ec8632ea78ac2ca35fd639300ada273
Michal Hozza <mhozza@gmail.com>
2012-05-27 22:32:54 +0200

    zmeny v trenovacich skriptoch

---
+++ NeuralNet/test_rnna_fft_tr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#echo "r $1 $2" | ./NeuralNet c 1 1 0 ../hand_images/test/hands_r_fft ../hand_images/test/other_r_fft train_rnn_$1_$2.weights
+echo "r $1" | ./NeuralNetRa c 1 1 0 ../hand_images/hands_r_fft ../hand_images/other_r_fft train_rnna_$1.weights
+
